a22d1c0b1dcb763b99fda3120a94d510019698f5
Alexandre Ruault <alexandre.ruault@blablacar.com>
2022-12-16 17:04:56 +0100

    BBC-22859 fixing some formatting & add todo instead of MetricTag

--- src/main/kotlin/com/comuto/androidtemplates/templates/standalone/klass/StandaloneActivity.kt
+++ src/main/kotlin/com/comuto/androidtemplates/templates/standalone/klass/StandaloneActivity.kt
@@ -12,26 +12,9 @@ package $packageName
 
 import com.comuto.databinding.${layoutName.toCamelCase().replace("_", "")}Binding
 import com.comuto.di.InjectHelper
-import com.comuto.coreui.PixarActivityV2
-import com.comuto.annotation.MetricTag
-import com.comuto.annotation.Architecture
-import com.comuto.annotation.BackgroundTaskManagement
-import com.comuto.annotation.DependencyInjection
-import com.comuto.annotation.DesignSystem
-import com.comuto.annotation.Flow
-import com.comuto.annotation.Modularized
-import com.comuto.annotation.Owner
 import android.os.Bundle
 
-@MetricTag(
-    owner = Owner.CHAPTER,
-    designSystem = DesignSystem.PIXAR,
-    dependencyInjection = DependencyInjection.DAGGER,
-    architecture = Architecture.NOT_APPLICABLE,
-    backgroundTaskManagement = BackgroundTaskManagement.NOT_APPLICABLE,
-    modularized = Modularized.NO,
-    flow = Flow.NOT_APPLICABLE
-)
+TODO() //@MetricTag()
 class $activityName() : PixarActivityV2() {
 
     private var _binding: ${layoutName.toCamelCase().replace("_", "")}Binding? = null
@@ -41,7 +24,7 @@ class $activityName() : PixarActivityV2() {
     override fun onCreate(savedInstanceState: Bundle?) {
         TODO() //Add this activity to a manifest
         super.onCreate(savedInstanceState)
-        _binding = ${layoutName.toCamelCase().replace("_", "")} Binding . inflate (layoutInflater)
+        _binding = ${layoutName.toCamelCase().replace("_", "")} Binding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
