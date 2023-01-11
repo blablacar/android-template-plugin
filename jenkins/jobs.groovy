e4f3d88ad5b7e3b975f2e356b66e942903b63060
Yohan BITTAN <yohan.bittan@blablacar.com>
2023-01-11 18:28:26 +0100

    WIP

--- jenkins/jobs.groovy
+++ jenkins/jobs.groovy
@@ -2,17 +2,6 @@ final String rootFolder = 'android-jobs/template-plugin'
 final String pipelinesFolder = 'jenkins'
 final String mainBranch = 'master'
 
-final String debugParameters = '''
-    parameters {
-        stringParam('branch', '', 'branch to publish, e.g. feature/BBC-XXXX or bugfix/BBC-XXXX')
-      }
-'''
-
-return [[name       : "$rootFolder/template-plugin-publish-debug",
-         jenkinsfile: "$pipelinesFolder/JenkinsfilePublishDebug",
-         branch     : mainBranch,
-         extra      : debugParameters],
-
-        [name       : "$rootFolder/template-plugin-publish-release",
+return [[name       : "$rootFolder/template-plugin-publish-release",
          jenkinsfile: "$pipelinesFolder/JenkinsfilePublishRelease",
          branch     : mainBranch]]
